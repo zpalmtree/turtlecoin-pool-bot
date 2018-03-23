8947b2cd99892e84430cdeaf42b5a1c506d18f3a
ZedPea <zp+github@zacharypalmer.co.uk>
2018-03-23 21:26:25 +0000

    Add some commands

--- Bot.go
+++ Bot.go
@@ -4,11 +4,18 @@ import (
     "github.com/bwmarrin/discordgo"
     "fmt"
     "os"
+    "os/signal"
+    "syscall"
     "bufio"
     "strings"
     "net/http"
     "encoding/json"
     "io/ioutil"
+    "regexp"
+    "strconv"
+    "errors"
+    "sort"
+    "time"
 )
 
 const poolsJSON string = "https://raw.githubusercontent.com/turtlecoin/" +
@@ -21,18 +28,189 @@ type Pool struct {
 
 type Pools map[string]*Pool
 
+var globalPools Pools
+var globalHeights map[string]int
+var globalHeight int
+
 func main() {
-    _, err := startup()
+    discord, err := startup()
     
     if err != nil {
         return
     }
 
-    pools, err := getPools()
+    globalPools, err = getPools()
+
+    fmt.Println(len(globalPools))
 
     if err != nil {
         return
     }
+
+    globalHeights = getHeights(globalPools)
+
+    globalHeight = median(getValues(globalHeights))
+
+    fmt.Println("Bot started!")
+
+    /* Update the height and pools in the background */
+    go heightWatcher()
+    go poolUpdater()
+
+    sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+
+    fmt.Println("Shutdown requested.")
+
+    discord.Close()
+
+    fmt.Println("Shutdown.")
+}
+
+/* Update the height and median height every 30 secs */
+func heightWatcher() {
+    for {
+        time.Sleep(time.Second * 30)
+
+        globalHeights = getHeights(globalPools)
+        globalHeight = median(getValues(globalHeights))
+    }
+}
+
+/* Update the pools json every hour */
+func poolUpdater() {
+    for {
+        time.Sleep(time.Hour)
+
+        tmpPools, err := getPools()
+        
+        if err != nil {
+            return
+        }
+
+        globalPools = tmpPools
+    }
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+    /* Ignore our own messages */
+    if m.Author.ID == s.State.User.ID {
+        return
+    }
+
+    if m.Content == ".heights" {
+        heightsPretty := "```\nAll known pool heights:\n\n"
+
+        for k, v := range globalHeights {
+            heightsPretty += fmt.Sprintf("%-25s %d\n", k, v)
+        }
+
+        heightsPretty += "```"
+
+        s.ChannelMessageSend(m.ChannelID, heightsPretty)
+
+        return
+    }
+
+    if m.Content == ".help" {
+        helpCommand := fmt.Sprintf("```\nAvailable commands:\n\n" +
+                   ".help           Display this help message\n" +
+                   ".heights        Display the heights of all known pools\n" +
+                   ".height         Display the median height of all pools\n" +
+                   ".height <pool>  Display the height of <pool>\n" +
+                   ".claim <pool>   Claim the pool <pool> as your pool```")
+
+
+        s.ChannelMessageSend(m.ChannelID, helpCommand)
+
+        return
+    }
+
+    if m.Content == ".height" {
+        s.ChannelMessageSend(m.ChannelID, 
+                             fmt.Sprintf("```Median pool height:\n\n%d```", 
+                                         globalHeight))
+
+        return
+    }
+}
+
+func getValues(heights map[string]int) []int {
+    values := make([]int, 0)
+
+    for _, v := range heights {
+        values = append(values, v)
+    }
+
+    return values
+}
+
+func median(heights []int) int {
+    sort.Ints(heights)
+
+    half := len(heights) / 2
+    median := heights[half]
+
+    if len(heights) % 2 == 0 {
+        median = (median + heights[half-1]) / 2
+    }
+
+    return median
+}
+
+func getHeights (pools Pools) map[string]int {
+    heights := make(map[string]int)
+
+    for _, v := range pools {
+        height, err := getPoolHeight(v.Api)
+
+        if err == nil {
+            heights[v.Url] = height
+        }
+    }
+
+    return heights
+}
+
+func getPoolHeight (apiURL string) (int, error) {
+    statsURL := apiURL + "stats"
+
+    resp, err := http.Get(statsURL)
+
+    if err != nil {
+        fmt.Printf("Failed to download stats from %s! Error: %s\n", 
+                    statsURL, err)
+        return 0, err
+    }
+
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+
+    if err != nil {
+        fmt.Printf("Failed to download stats from %s! Error: %s\n",
+                    statsURL, err)
+        return 0, err
+    }
+
+    re := regexp.MustCompile(".*\"height\":(\\d+).*")
+
+    height := re.FindStringSubmatch(string(body))
+
+    if len(height) < 2 {
+        fmt.Println("Failed to parse height from", statsURL)
+        return 0, errors.New("Couldn't parse height")
+    }
+
+    i, err := strconv.Atoi(height[1])
+
+    if err != nil {
+        fmt.Println("Failed to convert height into int! Error:", err)
+        return 0, err
+    }
+
+    return i, nil
 }
 
 /* Thanks to https://stackoverflow.com/a/48716447/8737306 */
@@ -96,12 +274,14 @@ func startup() (*discordgo.Session, error) {
         return discord, err
     }
 
-    discord, err = discordgo.New(string(token))
+    discord, err = discordgo.New("Bot " + token)
 
     if err != nil {
         fmt.Println("Failed to init bot! Error:", err)
         return discord, err
     }
+
+    discord.AddHandler(messageCreate)
 
     err = discord.Open()
 
